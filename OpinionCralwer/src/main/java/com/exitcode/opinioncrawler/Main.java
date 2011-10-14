7d304a34d203d2293aabd57200d38aa01cfde133
doc180 <degreeofchange180@gmail.com>
2011-10-14 11:59:10 -0500

    initial project setting

---
+++ OpinionCralwer/src/main/java/com/exitcode/opinioncrawler/Main.java
@@ -0,0 +1,12 @@
+package com.exitcode.opinioncrawler;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author: pradheepraju
+ * Date: Oct 14, 2011
+ * Time: 11:52:54 AM
+ * Welcome to Opinion crawler.
+ */
+public class Main {
+}
---
+++ OpinionCralwer/src/main/java/com/exitcode/opinioncrawler/crawler/MainCrawler.java
@@ -0,0 +1,31 @@
+package com.exitcode.opinioncrawler.crawler;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author: pradheepraju
+ * Date: Oct 14, 2011
+ * Time: 11:53:59 AM
+ * The Main type for performing crawling, the main step involved in crawling are
+ * 1- Seeding the urls.
+ * 2- Fetching the urls.
+ * 3- Parsing the fetched content.
+ * 4- Updating the fetched content
+ * 5- Make the fetched content searchable.
+ * <p/>
+ * The final content is the responsibility of the search module. Once we incorporated these things, we will have
+ * to expose the search content for our NLP module.
+ */
+public interface MainCrawler {
+
+
+    void performSeeding();
+
+    void performFetching();
+
+    void parseFetchedContent();
+
+    void updateFetchedContent();
+
+
+}
